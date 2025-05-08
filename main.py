81111c012c18846071da46712b2fdcbf0a615e35
Adrian Rutkowski <534104@student.saxion.nl>
2025-05-08 12:06:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import io
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
+from ultralytics import YOLO
+from PIL import Image
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = YOLO("model/my_model.pt")
+
+@app.post("/echo-image")
+async def echo_image(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    return StreamingResponse(io.BytesIO(image_bytes), media_type=file.content_type)
+    
+
+@app.post("/recognition")
+async def GetRecognizePieces(file:  UploadFile = File(...)):
+
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    result = model.predict(image)
+
+    detections = []
+    for r in result:
+        for box in r.boxes:
+            cls_id = int(box.cls[0].item())
+            label = model.names[cls_id]
+            conf = float(box.conf[0].item())
+            xyxy = box.xyxy[0].tolist()  # [x1, y1, x2, y2]
+            detections.append({
+                "label": label,
+                "confidence": conf,
+                "box": xyxy
+            })
+
+    return JSONResponse(content={"detections": detections})
