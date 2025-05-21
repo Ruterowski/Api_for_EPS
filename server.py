93925013c9af52dc9856e7d5daac62a36225ad36
Adrian Rutkowski <534104@student.saxion.nl>
2025-05-21 15:26:16 +0200

    Added 2nd model

--- server.py
+++ server.py
@@ -16,19 +16,44 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-model = YOLO("model/my_model.pt")
-
 @app.post("/echo-image")
 async def echo_image(file: UploadFile = File(...)):
     image_bytes = await file.read()
     return StreamingResponse(io.BytesIO(image_bytes), media_type=file.content_type)
     
 
-@app.post("/recognition")
-async def GetRecognizePieces(file:  UploadFile = File(...)):
+@app.post("/recognition/m1")
+async def GetRecognizePiecesModel1(file:  UploadFile = File(...)):
 
     image_bytes = await file.read()
     image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    model = YOLO("model/my_model1.pt")
+
+    result = model.predict(image)
+
+    detections = []
+    for r in result:
+        for box in r.boxes:
+            cls_id = int(box.cls[0].item())
+            label = model.names[cls_id]
+            conf = float(box.conf[0].item())
+            xyxy = box.xyxy[0].tolist()
+            detections.append({
+                "label": label,
+                "confidence": conf,
+                "box": xyxy
+            })
+
+    return JSONResponse(content={"detections": detections})
+
+@app.post("/recognition/m2")
+async def GetRecognizePiecesModel2(file:  UploadFile = File(...)):
+
+    image_bytes = await file.read()
+    image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+
+    model = YOLO("model/my_model2.pt")
 
     result = model.predict(image)
 
